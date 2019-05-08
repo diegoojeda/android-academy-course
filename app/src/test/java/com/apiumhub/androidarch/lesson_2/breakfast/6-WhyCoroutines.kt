09a38c4d57a9a1f1194284988670ab99ffcc880c
Diego Ojeda <diegoojedagarcia@gmail.com>
2019-05-08 11:36:04 +0200

    Fixed WhyCoroutines test

--- app/src/test/java/com/apiumhub/androidarch/lesson_2/breakfast/6-WhyCoroutines.kt
+++ app/src/test/java/com/apiumhub/androidarch/lesson_2/breakfast/6-WhyCoroutines.kt
@@ -1,9 +1,12 @@
 package com.apiumhub.androidarch.lesson_2.breakfast
 
+import junit.framework.TestCase.fail
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
+import java.io.File
 import java.io.FileInputStream
 import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 import kotlin.concurrent.thread
 
 class `6-WhyCoroutines` {
@@ -15,14 +18,17 @@ class `6-WhyCoroutines` {
     @Test
     fun fileProcessingWithThreadsAndCallbacks() {
         val countDownLatch = CountDownLatch(1)
-        val fos = FileInputStream("1-BreakfastSequentially.kt")
+        val fos = FileInputStream(FILE_NAME)
         getFileSizeInBackground(fos) {
             println("Finished processing file")
             println("Read $it bytes")
             countDownLatch.countDown()
         }
         fos.close()
-        countDownLatch.await()
+        countDownLatch.await(3, TimeUnit.SECONDS)
+        if (countDownLatch.count != 0L) {
+            fail()
+        }
     }
 
     private fun getFileSizeInBackground(file: FileInputStream, callback: (Int) -> Unit) {
@@ -39,7 +45,7 @@ class `6-WhyCoroutines` {
      */
     @Test
     fun fileProcessingWithCoroutines() = runBlocking {
-        val fos = FileInputStream("1-BreakfastSequentially.kt")
+        val fos = FileInputStream(FILE_NAME)
         val bytes = getFileSizeWithCoroutines(fos)
         fos.close()
         println("Finished processing file")
@@ -49,5 +55,9 @@ class `6-WhyCoroutines` {
     private suspend fun getFileSizeWithCoroutines(file: FileInputStream): Int {
         delay(1)
         return file.read()
+    }
+
+    companion object {
+        const val FILE_NAME = "example.txt"
     }
 }
