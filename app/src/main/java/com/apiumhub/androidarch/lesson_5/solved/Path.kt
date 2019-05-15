e67358e5823abc5827b20dfc8377e43456d90076
Diego Ojeda <diegoojedagarcia@gmail.com>
2019-05-15 17:10:11 +0200

    Removed return from computeDestination

--- app/src/main/java/com/apiumhub/androidarch/lesson_5/solved/Path.kt
+++ app/src/main/java/com/apiumhub/androidarch/lesson_5/solved/Path.kt
@@ -2,9 +2,8 @@ package com.apiumhub.androidarch.lesson_5.solved
 
 data class Path(val instructions: List<Instruction>) {
 
-    fun computeDestination(initialPosition: Position): Position {
-        return instructions.fold(initialPosition) { acc, next -> next.apply(acc) }
-    }
+    fun computeDestination(initialPosition: Position): Position =
+        instructions.fold(initialPosition) { acc, next -> next.apply(acc) }
 
 }
 
