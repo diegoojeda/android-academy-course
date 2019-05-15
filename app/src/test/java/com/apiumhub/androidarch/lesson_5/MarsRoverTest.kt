00e6e60f38b57e6e9781f7fb156c29024414067a
Diego Ojeda <diegoojedagarcia@gmail.com>
2019-05-15 16:22:42 +0200

    Mars rover kata added to project

---
+++ app/src/test/java/com/apiumhub/androidarch/lesson_5/MarsRoverTest.kt
@@ -0,0 +1,16 @@
+package com.apiumhub.androidarch.lesson_5
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+class MarsRoverTest {
+
+    @Test
+    fun should_compute_final_position_when_initial_position_and_path_provided() {
+        val marsRover = MarsRover()
+
+        val finalPosition = marsRover.compute("5 5\n1 2 N\nLMLMLMLMM")
+
+        assertThat(finalPosition).isEqualTo("1 3 N")
+    }
+}
