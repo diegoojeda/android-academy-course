7a9040ec83bcee17e208c91bd28aad4f4c65b50b
Diego Ojeda <diegoojedagarcia@gmail.com>
2019-05-16 11:03:21 +0200

    Removed computeDestination implementation

--- app/src/main/java/com/apiumhub/androidarch/lesson_5/Path.kt
+++ app/src/main/java/com/apiumhub/androidarch/lesson_5/Path.kt
@@ -3,7 +3,7 @@ package com.apiumhub.androidarch.lesson_5
 data class Path(private val instructions: List<Instruction>) {
 
     fun computeDestination(initialPosition: Position): Position {
-        return instructions.fold(initialPosition) { acc, instruction -> instruction.apply(acc) }
+        TODO()
     }
 
 }
