9b47cdc504f276b8a50376b89ce46aa618fe4115
Diego Ojeda <diegoojedagarcia@gmail.com>
2019-07-24 11:24:02 +0200

    Lesson 7

--- ui-testing/src/androidTest/java/com/apiumhub/ui_testing/lesson_7/HexPickerTest.kt
+++ ui-testing/src/androidTest/java/com/apiumhub/ui_testing/lesson_7/HexPickerTest.kt
@@ -1,8 +1,15 @@
 package com.apiumhub.ui_testing.lesson_7
 
+import android.graphics.drawable.ColorDrawable
+import android.view.View
+import android.widget.EditText
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.rule.ActivityTestRule
 import com.apiumhub.ui_testing.R
 import junit.framework.TestCase.assertEquals
+import org.junit.Assert.assertNotEquals
 import org.junit.Rule
 import org.junit.Test
 
@@ -17,5 +24,14 @@ class HexPickerTest {
         val actualColor: Int = component.getColor()
 
         assertEquals(expectedColor, actualColor)
+    }
+
+    @Test
+    fun should_change_color_when_hex_color_is_typed() {
+        val oldColor = (activityRule.activity.findViewById<View>(R.id.hexView).background as? ColorDrawable)?.color ?: 0
+        onView(withId(R.id.hexEt)).perform(typeText("#000000"))
+        val actualColor = (activityRule.activity.findViewById<View>(R.id.hexView).background as? ColorDrawable)?.color ?: 0
+
+        assertNotEquals(oldColor, actualColor)
     }
 }
--- ui-testing/src/main/java/com/apiumhub/ui_testing/lesson_7/HexPicker.kt
+++ ui-testing/src/main/java/com/apiumhub/ui_testing/lesson_7/HexPicker.kt
@@ -1,14 +1,19 @@
 package com.apiumhub.ui_testing.lesson_7
 
 import android.content.Context
+import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
 import android.widget.FrameLayout
 import androidx.annotation.ColorRes
 import androidx.core.content.ContextCompat
 import com.apiumhub.ui_testing.R
 import kotlinx.android.synthetic.main.hex_picker.view.*
+import java.lang.NumberFormatException
 
 class HexPicker @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
     FrameLayout(context, attrs, defStyleAttr) {
@@ -24,5 +29,25 @@ class HexPicker @JvmOverloads constructor(context: Context, attrs: AttributeSet?
         attributes.recycle()
 
         hexView.setBackgroundColor(color)
+
+        hexEt.addTextChangedListener(SimpleTextWatcher {
+            try {
+                hexView.setBackgroundColor(Color.parseColor(it))
+            } catch (ex: Exception) {
+                Log.e("Component", "Invalid color")
+            }
+        })
+    }
+}
+
+class SimpleTextWatcher(private val textChanged: (String) -> Unit): TextWatcher {
+    override fun afterTextChanged(s: Editable?) {
+        textChanged(s.toString())
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
     }
 }
